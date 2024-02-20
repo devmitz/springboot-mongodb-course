8a1354f68c78302cd6c30f89b233253914345445
Felipe Schmitz <contato.felipeschmitz@gmail.com>
2024-02-20 15:17:53 -0300

    Getting a user by ID

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,19 +1,27 @@
 package com.example.demo.services;
 
 import com.example.demo.domain.User;
+import com.example.demo.exception.ObjectNotFoundException;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    UserRepository repository;
+    private UserRepository repository;
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> optionalUser = repository.findById(id);
+        User user = optionalUser.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+        return user;
     }
 }

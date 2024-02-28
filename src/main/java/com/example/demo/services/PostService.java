c556ddefd93e8033e5476e94a840b4e9f4c21115
Felipe Schmitz <contato.felipeschmitz@gmail.com>
2024-02-28 12:19:02 -0300

    Query with multiple criteria

--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -9,6 +9,7 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,4 +29,8 @@ public class PostService {
         return repository.searchTitle(text);
     }
 
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
+    }
 }
